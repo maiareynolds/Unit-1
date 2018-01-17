047d071da8bfff5b4b38dd2adbf61fadd74d8728
maiareynolds <35492770+maiareynolds@users.noreply.github.com>
2018-01-17 17:12:36 -0500

    Updated from Brython Server: 1/17/2018 5:12:35 PM

--- slope.py
+++ slope.py
@@ -1,1 +1,9 @@
+#Maia Reynolds
+#1/17/18
+#slope.py - calculates slope of line given two points
 
+point1=input("First point: ")
+x1,y1=point1.split(",")
+point2=input("Second point: ")
+x2,y2=point2.split(",")
+slope=(y1-y2)/(x1-x2)
