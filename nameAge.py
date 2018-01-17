f2139d3176abf07c3e9c607401fdfd566ccb1083
maiareynolds <35492770+maiareynolds@users.noreply.github.com>
2018-01-17 08:39:07 -0500

    Updated from Brython Server: 1/17/2018 8:39:06 AM

--- nameAge.py
+++ nameAge.py
@@ -1,1 +1,8 @@
+#Maia Reynolds
+#1/17/18
+#nameAge.py - characters in names and age next year
 
+fullName=input("Enter your first and last name: ")
+input("Enter your age: ")
+first,last=fullName.split( )
+print("Your first name has",len(first),"letters")
