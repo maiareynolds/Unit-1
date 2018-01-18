f208c393052bf656b049c9ac81b0fad7af7b2021
maiareynolds <35492770+maiareynolds@users.noreply.github.com>
2018-01-17 19:34:30 -0500

    Updated from Brython Server: 1/17/2018 7:34:30 PM

--- stringAnalysis.py
+++ stringAnalysis.py
@@ -1,1 +1,17 @@
+#Maia Reynolds
+#1/17/18
+#stringAnalysis.py - number of words and characters in a sentence, searching for words and characters
 
+sentence=input("Enter a sentence: ")
+characters=len(sentence)
+spaces=int((sentence.count(" ")))
+periods=int((sentence.count(".")))
+questionMarks=int((sentence.count("?")))
+commas=int((sentence.count(",")))
+dashes=int((sentence.count("-")))
+apostrophes=int((sentence.count("'")))
+semicolons=int((sentence.count(";")))
+colons=int((sentence.count(":")))
+exMark=int((sentence.count("!")))
+letters=int(characters-(spaces+periods+questionMarks+commas+dashes+apostrophes+semicolons+colons+exMark))
+print("Your sentence has ",spaces+1,"words and ",characters,"characters and ",letters,"letters")
