f80bb016071a274bd86680e0dbc7779d44f100c8
maiareynolds <35492770+maiareynolds@users.noreply.github.com>
2018-01-19 17:22:07 -0500

    Updated from Brython Server: 1/19/2018 5:22:06 PM

--- isItaTriangle.py
+++ isItaTriangle.py
@@ -1,1 +1,11 @@
+#Maia Reynolds
+#1/19/18
+#isItaTriangle.py - determines if it could be a triangle
 
+a=float(input("Enter Side A: "))
+b=float(input("Enter Side B: "))
+c=float(input("Enter Side C: "))
+
+large=max(a,b,c)
+smaller=((a+b+c)-large)
+print(bool(smaller>large))
