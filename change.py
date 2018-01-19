58473bdf5574dc3623d742315eed8e4cef4a18f3
maiareynolds <35492770+maiareynolds@users.noreply.github.com>
2018-01-19 08:44:11 -0500

    Updated from Brython Server: 1/19/2018 8:44:10 AM

--- change.py
+++ change.py
@@ -1,1 +1,10 @@
+#Maia Reynolds
+#1/19/18
+#change.py - amount of money to change
 
+cents=int(input("number of cents: "))
+print("Quarters: ",int(cents//25))
+Dimes=(int(cents%25)//10)
+print("Dimes: ",Dimes)
+Nickels=(int(int(cents%25)//10
+print("Nickels: ",
