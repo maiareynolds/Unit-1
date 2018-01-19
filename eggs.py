2dbb9e7c75c1bd6960ea6248ee8e066f0464dca0
maiareynolds <35492770+maiareynolds@users.noreply.github.com>
2018-01-19 08:32:56 -0500

    Updated from Brython Server: 1/19/2018 8:32:55 AM

--- eggs.py
+++ eggs.py
@@ -1,1 +1,7 @@
+#Maia Reynolds
+#1/19/18
+#eggs.py - #eggs to #cartons true or false
 
+eggs=int(input("Number of Eggs= "))
+cartons=int(input("Number of Egg Cartons= "))
+print(bool(cartons==eggs/12))
