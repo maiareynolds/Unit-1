4cbb28464259b08711d58f9df78b8ae9f2c87328
maiareynolds <35492770+maiareynolds@users.noreply.github.com>
2018-01-25 08:48:20 -0500

    Updated from Brython Server: 1/25/2018 8:48:18 AM

--- triangleAnglesArea.py
+++ triangleAnglesArea.py
@@ -9,3 +9,9 @@ a=int(input("Enter side A: "))
 b=int(input("Enter side B: "))
 c=int(input("Enter side C: "))
 degrees(
+angleA=cos(min(a,b,c)/max(a,b,c))
+
+print("Angle A is",,"degrees")
+print("Angle B is",,"degrees")
+print("Angle C is",,"degrees")
+print("The area of triangle ABC is",((.5*max(a,b,c))*(asin(SMALLEST ANGLE)*min(a,b,c))))
