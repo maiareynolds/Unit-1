fdbdb5a61715a7c9e473a50427df347ac4bf4636
maiareynolds <35492770+maiareynolds@users.noreply.github.com>
2018-01-25 08:23:09 -0500

    Updated from Brython Server: 1/25/2018 8:23:08 AM

--- distance2points.py
+++ distance2points.py
@@ -1,1 +1,10 @@
+#Maia Reynolds
+#1/25/18
+#distance2points.py - finds distance between two points
 
+from math import sqrt
+x1=int(input("Enter x1: "))
+y1=int(input("Enter y1: "))
+x2=int(input("Enter x2: "))
+y2=int(input("Enter y2: "))
+print("The Distance is ",sqrt(abs((x1-x2)**2)+abs((y1-y2)**2)))
