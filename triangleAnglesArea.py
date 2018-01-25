c8fdd469a25dc049027d3881b1c4e0712e89cc63
maiareynolds <35492770+maiareynolds@users.noreply.github.com>
2018-01-25 08:40:53 -0500

    Updated from Brython Server: 1/25/2018 8:40:52 AM

--- triangleAnglesArea.py
+++ triangleAnglesArea.py
@@ -1,1 +1,11 @@
+#Maia Reynolds
+#1/25/18
+#triangleAnglesArea.py - tells angles and area of triangle
 
+from math import cos
+from math import asin
+from math import degrees
+a=int(input("Enter side A: "))
+b=int(input("Enter side B: "))
+c=int(input("Enter side C: "))
+degrees(
