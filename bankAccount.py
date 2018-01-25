71374a76ec00c0040e975ab86229dc01f6e4f158
maiareynolds <35492770+maiareynolds@users.noreply.github.com>
2018-01-25 08:35:21 -0500

    Updated from Brython Server: 1/25/2018 8:35:20 AM

--- bankAccount.py
+++ bankAccount.py
@@ -1,1 +1,10 @@
+#Maia Reynolds
+#1/25/18
+#bankAccount.py - finds number of years for money to reach certain amount
 
+from math import log10
+start=int(input("Enter starting value: "))
+interest=float(input("Enter interest rate as a decimal: "))
+compound=int(input("Enter how many times per year interest is compounded: "))
+final=float(input("Enter the final amount of money you want: "))
+print("It will take",(log10(final/start)/log10(1+(interest/compound)))/compound,"years")
